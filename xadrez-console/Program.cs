591cd8879aa6db7c1af4181b2a70872b8ec28b9d
Jonathan <jo.f.a.snathan@gmail.com>
2024-10-04 15:43:41 -0300

    Controlando as pecas capturadas

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -18,11 +18,7 @@ internal class Program
                 {
 
                     Console.Clear();
-                    Tela.ImprimirTabuleiro(partida.Tab);
-
-                    Console.WriteLine();
-                    Console.WriteLine("Turno: " + partida.Turno);
-                    Console.WriteLine("Aguardando jogada: " + partida.JogadorAtual);
+                    Tela.imprimirPartida(partida);
 
                     Console.WriteLine();
                     Console.Write("Origem: ");
